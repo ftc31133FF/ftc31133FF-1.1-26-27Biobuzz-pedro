d1406ef1dcd3f1e2aed61ffb11085d4daaa75264
colewalker554 <colewalker554@gmail.com>
2026-08-25 23:09:05 -0500

    Hello World

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helloWorld.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp
+public class helloWorld extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addLine("Hell World!");
+    }
+}
