52adca156d8c5522b26e4d1f5b05a2c5a612a32b
colewalker554 <colewalker554@gmail.com>
2026-09-10 21:55:50 -0500

    cleaned up square auto, added additional path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testauto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testauto.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
@@ -37,60 +38,122 @@ public class testauto extends OpMode {
     }
 
     PathState pathState;
-
-    private final Pose startPose = new Pose(72, 24, Math.toRadians(135));
-
+/*
+    private final Pose startPosed = new Pose(72, 24, Math.toRadians(135));
     private final Pose checkPoint = new Pose(24, 72, Math.toRadians(135));
     private final Pose endPose = new Pose(75, 120, Math.toRadians(270));
 
-    private PathChain driveOne, MainChain;
+ */
+    private final Pose startPose = new Pose(60, 9, Math.toRadians(0));
+
+    private final double boxRadius = 54;
+    public final double cornerRadius = 36;
+    private final Pose bottomRight = new Pose(72 + boxRadius, 72 - boxRadius, Math.toRadians(0));
+    private final Pose topRight = new Pose(72 + boxRadius, 72 + boxRadius, Math.toRadians(0));
+    private final Pose topLeft = new Pose(72 - boxRadius, 72 + boxRadius, Math.toRadians(0));
+    private final Pose bottomLeft = new Pose(72 - boxRadius, 72 - boxRadius, Math.toRadians(0));
+
+    private PathChain driveOne, MainChain, initialLineUp, MainChainButFancy;
 
     public DcMotorEx intake;
 
 
 
     public void buildPaths() {
+        /*
         driveOne = follower.pathBuilder()
                 .addPath(new BezierLine(startPose, checkPoint))
                 .setTangentHeadingInterpolation()
                 .addPath(new BezierLine(checkPoint, endPose))
                 .setLinearHeadingInterpolation(checkPoint.getHeading(), endPose.getHeading())
                 .build();
+
+         */
+        initialLineUp = follower.pathBuilder()
+                .addPath(new BezierLine(
+                        startPose, new Pose(78, 18)
+                ))
+                .setConstantHeadingInterpolation(0)
+                .build();
+
         MainChain = follower.pathBuilder()
-                .addPath(
-                        new BezierLine(
-                                new Pose(24.000, 24.000),
-                                new Pose(120, 24.000)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                .addPath(
-                        new BezierLine(
-                                new Pose(120, 24.000),
-                                new Pose(120, 124)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                .addPath(
-                        new BezierLine(
-                                new Pose(120, 120),
-                                new Pose(24, 120)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                .addPath(
-                        new BezierLine(
-                                new Pose(24, 120.396),
-                                new Pose(24, 24)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+
+                .addPath(new BezierLine(
+                                new Pose(126, 18), new Pose(126, 126)
+                        ))
+                .setTangentHeadingInterpolation()
+
+                .addPath(new BezierLine(
+                                new Pose(126, 126), new Pose(18, 126)
+                        ))
+                .setTangentHeadingInterpolation()
+
+                .addPath(new BezierLine(
+                                new Pose(18, 126), new Pose(18, 18)
+                        ))
+                .setTangentHeadingInterpolation()
+
+                .addPath(new BezierLine(
+                                new Pose(18, 18), new Pose(126, 18)
+                        ))
+                .setTangentHeadingInterpolation()
+
+                .build();
+        MainChainButFancy = follower.pathBuilder()
+
+                .addPath(new BezierCurve(
+                        new Pose(bottomRight.getX() - cornerRadius, bottomRight.getY()), bottomRight, new Pose(bottomRight.getX(), bottomRight.getY() + cornerRadius)
+                ))
+                .setTangentHeadingInterpolation()
+
+                .addPath(new BezierLine(
+                        new Pose(bottomRight.getX(), bottomRight.getY() + cornerRadius), new Pose(topRight.getX(), topRight.getY() - cornerRadius)
+                ))
+                .setTangentHeadingInterpolation()
+
+
+
+                .addPath(new BezierCurve(
+                        new Pose(topRight.getX(), topRight.getY() - cornerRadius), topRight, new Pose(topRight.getX() - cornerRadius, topRight.getY())
+                ))
+                .setTangentHeadingInterpolation()
+
+                .addPath(new BezierLine(
+                        new Pose(topRight.getX() - cornerRadius, topRight.getY()), new Pose(topLeft.getX() + cornerRadius, topLeft.getY())
+                ))
+                .setTangentHeadingInterpolation()
+
+
+
+                .addPath(new BezierCurve(
+                        new Pose(topLeft.getX() + cornerRadius, topLeft.getY()), topLeft, new Pose(topLeft.getX(), topLeft.getY() - cornerRadius)
+                ))
+                .setTangentHeadingInterpolation()
+
+                .addPath(new BezierLine(
+                        new Pose(topLeft.getX(), topLeft.getY() - cornerRadius), new Pose(bottomLeft.getX(), bottomLeft.getY() + cornerRadius)
+                ))
+                .setTangentHeadingInterpolation()
+
+
+
+                .addPath(new BezierCurve(
+                        new Pose(bottomLeft.getX(), bottomLeft.getY() + cornerRadius), bottomLeft, new Pose(bottomLeft.getX() + cornerRadius, bottomLeft.getY())
+                ))
+                .setTangentHeadingInterpolation()
+
+                .addPath(new BezierLine(
+                        new Pose(bottomLeft.getX() + cornerRadius, bottomLeft.getY()), new Pose(bottomRight.getX() - cornerRadius, bottomRight.getY())
+                ))
+                .setTangentHeadingInterpolation()
+
                 .build();
     }
 
     public void statePathUpdate() {
         switch(pathState) {
             case DRIVE:
+                follower.followPath(initialLineUp, true);
                 follower.followPath(MainChain, true);
                 setPathState(PathState.END);
                 break;
